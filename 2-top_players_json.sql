ed4c66e5e4e008ad39a37e2e7e23c08a82ff79c7
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-04 20:33:11 +0100

    Day 2 challenge completed

--- 2-top_players_json.sql
+++ 2-top_players_json.sql
@@ -4,14 +4,14 @@
 USE my_db;
 
 SELECT 'highest_goal_scorer' AS title, JSON_OBJECT('id', id, 'nationality', nationality, 'fifa_ranking', fifa_ranking,
-																 'national_team_kit_sponsor', national_team_kit_sponsor, 'position', position,
-                                                                 'national_team_jersey_number', national_team_jersey_number, 'player_dob', player_dob,
-                                                                 'club', club, 'player_name', player_name, 'appearances', appearances,
-                                                                 'goals_scored', goals_scored, 'assists_provided', assists_provided,
-                                                                 'dribbles_per_90', dribbles_per_90, 'interceptions_per_90', interceptions_per_90,
-                                                                 'tackles_per_90', tackles_per_90, 'total_duels_won_per_90', total_duels_won_per_90,
-                                                                 'save_percentage', save_percentage, 'clean_sheets', clean_sheets,
-                                                                 'brand_sponsorbrand_used', brand_sponsorbrand_used) AS player
+												   'national_team_kit_sponsor', national_team_kit_sponsor, 'position', position,
+                                                   'national_team_jersey_number', national_team_jersey_number, 'player_dob', player_dob,
+                                                   'club', club, 'player_name', player_name, 'appearances', appearances,
+                                                   'goals_scored', goals_scored, 'assists_provided', assists_provided,
+                                                   'dribbles_per_90', dribbles_per_90, 'interceptions_per_90', interceptions_per_90,
+                                                   'tackles_per_90', tackles_per_90, 'total_duels_won_per_90', total_duels_won_per_90,
+                                                   'save_percentage', save_percentage, 'clean_sheets', clean_sheets,
+                                                   'brand_sponsorbrand_used', brand_sponsorbrand_used) AS player
   FROM wc_players
   WHERE goals_scored = (SELECT MAX(goals_scored)
 						  FROM wc_players
@@ -19,27 +19,27 @@ SELECT 'highest_goal_scorer' AS title, JSON_OBJECT('id', id, 'nationality', nati
 ;
 
 SELECT 'highest_assists' AS title, JSON_OBJECT('id', id, 'nationality', nationality, 'fifa_ranking', fifa_ranking,
-																 'national_team_kit_sponsor', national_team_kit_sponsor, 'position', position,
-                                                                 'national_team_jersey_number', national_team_jersey_number, 'player_dob', player_dob,
-                                                                 'club', club, 'player_name', player_name, 'appearances', appearances,
-                                                                 'goals_scored', goals_scored, 'assists_provided', assists_provided,
-                                                                 'dribbles_per_90', dribbles_per_90, 'interceptions_per_90', interceptions_per_90,
-                                                                 'tackles_per_90', tackles_per_90, 'total_duels_won_per_90', total_duels_won_per_90,
-                                                                 'save_percentage', save_percentage, 'clean_sheets', clean_sheets,
-                                                                 'brand_sponsorbrand_used', brand_sponsorbrand_used) AS player
+											   'national_team_kit_sponsor', national_team_kit_sponsor, 'position', position,
+                                               'national_team_jersey_number', national_team_jersey_number, 'player_dob', player_dob,
+                                               'club', club, 'player_name', player_name, 'appearances', appearances,
+                                               'goals_scored', goals_scored, 'assists_provided', assists_provided,
+                                               'dribbles_per_90', dribbles_per_90, 'interceptions_per_90', interceptions_per_90,
+                                               'tackles_per_90', tackles_per_90, 'total_duels_won_per_90', total_duels_won_per_90,
+                                               'save_percentage', save_percentage, 'clean_sheets', clean_sheets,
+                                               'brand_sponsorbrand_used', brand_sponsorbrand_used) AS player
   FROM wc_players
   WHERE assists_provided = (SELECT MAX(assists_provided)
 							  FROM wc_players
                               WHERE assists_provided REGEXP '[0-9]');
 SELECT 'overall_best_player' AS title, JSON_OBJECT('id', id, 'nationality', nationality, 'fifa_ranking', fifa_ranking,
-																 'national_team_kit_sponsor', national_team_kit_sponsor, 'position', position,
-                                                                 'national_team_jersey_number', national_team_jersey_number, 'player_dob', player_dob,
-                                                                 'club', club, 'player_name', player_name, 'appearances', appearances,
-                                                                 'goals_scored', goals_scored, 'assists_provided', assists_provided,
-                                                                 'dribbles_per_90', dribbles_per_90, 'interceptions_per_90', interceptions_per_90,
-                                                                 'tackles_per_90', tackles_per_90, 'total_duels_won_per_90', total_duels_won_per_90,
-                                                                 'save_percentage', save_percentage, 'clean_sheets', clean_sheets,
-                                                                 'brand_sponsorbrand_used', brand_sponsorbrand_used) AS player
+												   'national_team_kit_sponsor', national_team_kit_sponsor, 'position', position,
+                                                   'national_team_jersey_number', national_team_jersey_number, 'player_dob', player_dob,
+                                                   'club', club, 'player_name', player_name, 'appearances', appearances,
+                                                   'goals_scored', goals_scored, 'assists_provided', assists_provided,
+                                                   'dribbles_per_90', dribbles_per_90, 'interceptions_per_90', interceptions_per_90,
+                                                   'tackles_per_90', tackles_per_90, 'total_duels_won_per_90', total_duels_won_per_90,
+                                                   'save_percentage', save_percentage, 'clean_sheets', clean_sheets,
+                                                   'brand_sponsorbrand_used', brand_sponsorbrand_used) AS player
   FROM wc_players
   WHERE goals_scored + assists_provided = (SELECT MAX(goals_scored + assists_provided)
 							  FROM wc_players
