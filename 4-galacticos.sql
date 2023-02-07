033a2f264c2e4a70028fdbb9cbd2b998d5604c7f
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-07 02:15:20 +0100

    Day 4 challenge completed

--- 4-galacticos.sql
+++ 4-galacticos.sql
@@ -5,11 +5,11 @@ USE my_db;
 SELECT nationality AS team,
 COUNT(player_name) As galacticos
 FROM wc_players
-WHERE goals_scored / appearances >= 0.25
+WHERE CAST(goals_scored AS unsigned)/ CAST(appearances AS unsigned) >= 0.25
 GROUP BY nationality;
 
 SELECT nationality AS team,
 JSON_ARRAY(GROUP_CONCAT(player_name)) As galacticos
 FROM wc_players
-WHERE goals_scored / appearances >= 0.25
+WHERE CAST(goals_scored AS unsigned)/ CAST(appearances AS unsigned) >= 0.25
 GROUP BY nationality;
